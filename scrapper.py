cf766989636e64554d744185f5a17d9b29d82501
Abdelbaki GUIR <96337336+abdou1579@users.noreply.github.com>
2024-07-16 18:57:49 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,23 +1,24 @@
-from twscrape import API, gather
-from twscrape.logger import set_log_level
+import argparse
 import re
 import pandas as pd
+from twscrape import API, gather
+import asyncio
 
-async def scrap_tweets(credential, users_path, text_limit):
-    with open(credential, 'r') as file:
+async def scrap_tweets(credentials_file, users_path, text_limit):
+    with open(credentials_file, 'r') as file:
         credentials = []
         for line in file:
             parts = line.strip().split(':')
-            credentials.extend([parts])
+            credentials.append(parts)
 
     with open(users_path, 'r') as file:
         data = file.read()
     match = re.search(r'users\s*=\s*(\[.*\])', data, re.DOTALL)
     if match:
         users_list_str = match.group(1)
-        users_list = eval(users_list_str) 
+        users_list = eval(users_list_str)
     else:
-        users_list = []
+    	users_list = []
 
     df = pd.DataFrame(columns=['tweet_id', 'user','created_at', 'post_text','lang','ViewCount','quoteCount','likeCount','replyCount','retweetCount'
 ,'bookmarkCount' ,'is_retweet','is_quote','reply_to_id','reply_to_user','Original tweets', 'reply_to_text', 'is_mutual_followership'])
@@ -104,3 +105,20 @@ async def scrap_tweets(credential, users_path, text_limit):
                 df = pd.concat([df, pd.DataFrame(chunk)])
             else: pass
     return df
+
+async def main(credentials_file, users_path, text_limit, path_to_save):
+    df = await scrap_tweets(credentials_file, users_path, int(text_limit))
+    df.to_csv(path_to_save, index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('credentials', metavar='FILE', type=str, help='Path to the credentials text file')
+    parser.add_argument('users_path', metavar='FILE', type=str, help='Path to the users list file')
+    parser.add_argument('text_limit', metavar='LIMIT', type=int, help='Text limit')
+    parser.add_argument('path_to_save', metavar='FILE', type=str, help='Path to save the generated scraping file')
+
+    args = parser.parse_args()
+
+    
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args.credentials, args.users_path, args.text_limit, args.path_to_save))
